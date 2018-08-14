9db365c08307ea9e7e41041f9056862eb8557e2a
Kevin Ahn and Nate Cottle <nate@>
2018-08-14 09:14:31 -0700

    Add initial commit with project layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Recipe Box';
+  recipes: Recipe[] = [
+    new Recipe('Mac n\' Cheese', ['Mac', 'Cheese'], ['Put mac in boiling water', 'let mac boil', 'remove water', 'add cheese', 'eat and enjoy']),
+    new Recipe('Bacon', ['Bacon'], ['Cook bacon.']),
+    new Recipe('Steamed Brocolli', ['Steam', 'brocolli'], ['Steam the brocolli.']),
+    new Recipe('Cereal', ['Milk', 'cereal', 'eraser shavings'], ['Put all ingredients into bowl and enjoy.'])
+  ];
 }
